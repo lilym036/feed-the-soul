39aa093107ed8437b2101ee4a546fa672010fe3b
Lilibeth Neal <mejialily036@gmail.com>
2024-02-28 16:24:25 -0600

    added routes

--- index.js
+++ index.js
@@ -10,5 +10,34 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(Quote.getQuote({author: false}));
 console.log(randomVerse());
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/quote", (req, res) => {
+    console.log(Quote.getQuote({author: false}));
+    let randomQuote= Quote.getQuote({author: false});
+
+    res.render("index.ejs", {
+        randomQuote: randomQuote,
+    });
+
+
+});
+
+app.post("/verse", (req, res) => {
+    console.log(randomVerse());
+    let randomVerse= randomVerse();
+
+    res.render("index.ejs", {
+        randomVerse: randomVerse;
+    });
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
